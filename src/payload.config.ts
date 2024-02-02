8be0168b3b3325c7c7b6c210b2703202d7d719e1
Fierce-Cat <hyx5020@gmail.com>
2024-02-01 21:49:00 -0800

    feat: add content field to Posts collection

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import { payloadCloud } from '@payloadcms/plugin-cloud'
 import { mongooseAdapter } from '@payloadcms/db-mongodb' // database-adapter-import
 import { webpackBundler } from '@payloadcms/bundler-webpack' // bundler-import
-import { slateEditor } from '@payloadcms/richtext-slate' // editor-import
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { buildConfig } from 'payload/config'
 
 import Users from './collections/Users'
@@ -14,7 +14,7 @@ export default buildConfig({
     user: Users.slug,
     bundler: webpackBundler(), // bundler-config
   },
-  editor: slateEditor({}), // editor-config
+  editor: lexicalEditor({}), // editor-config
   collections: [Users, Posts],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
